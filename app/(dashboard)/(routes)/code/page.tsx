fe5c9e5fe6a7c21c5e3bb833467610d2500da124
Liam Arbuckle <liam@skinetics.tech>
2023-09-30 09:48:52 +1000

    🐉🪨 ↝ Look into my eyes and tell me

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -8,6 +8,7 @@ import { useForm } from "react-hook-form";
 import axios from 'axios';
 import { ChatCompletionRequestMessage } from "openai";
 import * as z from "zod";
+import ReactMarkdown from 'react-markdown';
 
 import { Heading } from "@/components/Heading";
 import { formSchema } from "@/app/(dashboard)/(routes)/conversation/constants";
@@ -112,7 +113,21 @@ const CodePage = () => {
                             className={cn('p-8 w-full flex items-start gap-x-8 rounded-lg', message.role === 'user' ? 'bg-white border border-black/10' : 'bg-muted')}
                         >
                             {message.role === 'user' ? <UserAvatar /> : <BotAvatar />}
-                            <p className="text-sm">{message.content}</p>
+                            <ReactMarkdown
+                                components={{
+                                    pre: ({ node, ...props }) => (
+                                        <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg">
+                                            <pre {...props} />
+                                        </div>
+                                    ),
+                                    code: ({ node, ...props }) => (
+                                        <code className="bg-black/10 rounded-lg p-1" {...props} />
+                                    )
+                                }}
+                                className='text-sm overflow-hidden leading-7'
+                            >
+                                {message.content || ""}
+                            </ReactMarkdown>
                         </div>
                     ))}
                 </div>
