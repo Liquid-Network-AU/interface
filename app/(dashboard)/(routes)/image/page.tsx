77759fd817be6458a7d2ea39ea56e161650273f4
Liam Arbuckle <liam@skinetics.tech>
2023-10-14 10:44:36 +1100

    👞🌈 ↝ One more sunrise

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -1,124 +1,199 @@
-"use client"
+"use client";
 
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Image } from "lucide-react";
-import { useForm } from "react-hook-form";
-import axios from 'axios';
-import { ChatCompletionRequestMessage } from "openai";
 import * as z from "zod";
+import axios from "axios";
+import Image from "next/image";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Download, ImageIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 import { Heading } from "@/components/Heading";
-import { formSchema } from "@/app/(dashboard)/(routes)/conversation/constants";
-import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Card, CardFooter } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button"
-import { Empty } from "@/components/Empty";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Loader } from "@/components/Loader";
-import { cn } from "@/lib/utils";
-import { UserAvatar } from "@/components/User/Avatar";
-import { BotAvatar } from "@/components/User/BotAvatar";
+import { Empty } from "@/components/Empty";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+// import { useProModal } from "@/hooks/use-pro-modal";
 
-const ImagePage = () => {
-    const router = useRouter();
-    const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([])
+import { amountOptions, formSchema, resolutionOptions } from "./constants";
 
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            prompt: "",
-        }
-    })
+const PhotoPage = () => {
+//   const proModal = useProModal();
+  const router = useRouter();
+  const [photos, setPhotos] = useState<string[]>([]);
 
-    const isLoading = form.formState.isSubmitting;
-    const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        try {
-            const userMessage: ChatCompletionRequestMessage = {
-                role: "user",
-                content: values.prompt,
-            };
-            
-            const newMessages = [...messages, userMessage];
-            const response = await axios.post('/api/conversation', {
-                messages: newMessages,
-            });
-
-            setMessages((current) => [...current, userMessage, response.data]);
-            form.reset();
-        } catch (error: any) {
-            console.log(error);
-        } finally {
-            router.refresh();
-        }
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+      amount: "1",
+      resolution: "512x512"
     }
+  });
 
-    return (
-        <div>
-            <Heading
-                title="Image Generation"
-                description="Generate images using a custom proompter"
-                Icon={Image}
-                iconColor="text-pink-700"
-                bgColor="bg-pink-700/10"
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setPhotos([]);
+
+      const response = await axios.post('/api/image', values);
+
+      const urls = response.data.map((image: { url: string }) => image.url);
+
+      setPhotos(urls);
+    } catch (error: any) {
+      if (error?.response?.status === 403) {
+        // proModal.onOpen();
+      } else {
+        toast.error("Algo não está certo.");
+      }
+    } finally {
+      router.refresh();
+    }
+  }
+
+  return ( 
+    <div>
+      <Heading
+        title="Gerar uma Imagem"
+        description="Transforme oque escrever em imagens."
+        Icon={ImageIcon}
+        iconColor="text-pink-700"
+        bgColor="bg-pink-700/10"
+      />
+      <div className="px-4 lg:px-8">
+        <Form {...form}>
+          <form 
+            onSubmit={form.handleSubmit(onSubmit)} 
+            className="
+              rounded-lg 
+              border 
+              w-full 
+              p-4 
+              px-3 
+              md:px-6 
+              focus-within:shadow-sm
+              grid
+              grid-cols-12
+              gap-2
+            "
+          >
+            <FormField
+              name="prompt"
+              render={({ field }) => (
+                <FormItem className="col-span-12 lg:col-span-6">
+                  <FormControl className="m-0 p-0">
+                    <Input
+                      className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                      disabled={isLoading} 
+                      placeholder="A picture of a horse in Swiss alps" 
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
             />
-            <div className="px-4 lg:px-8">
-                <Form {...form}>
-                    <form
-                        onSubmit={form.handleSubmit(onSubmit)}
-                        className="
-                            rounded-lg
-                            border
-                            w-full
-                            p-4
-                            px-3
-                            md:px-6
-                            focus-within:shadow-sm
-                            grid
-                            grid-cols-12
-                            gap-2
-                        "
-                    >
-                        <FormField
-                            name='prompt'
-                            render={({ field }) => (
-                                <FormItem className="col-span-12 lg:col-span-10">
-                                    <FormControl className="m-0 p-0">
-                                        <Input
-                                            className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
-                                            disabled={isLoading}
-                                            placeholder="How do I calculate the radius of a pebis?"
-                                            {...field}
-                                        />
-                                    </FormControl>
-                                </FormItem>
-                            )}
-                        />
-                        <Button className="col-span-12 col-span-2" disabled={isLoading}>Generate</Button>
-                    </form>
-                </Form>
-            </div>
-            <div className="space-y-4 mt-4">
-                {isLoading && (
-                    <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted"><Loader /></div>
-                )}
-                {messages.length === 0 && !isLoading && (
-                    <Empty label="No conversations started" />
-                )}
-                <div className="flex flex-col-reverse gap-y-4">
-                    {messages.map((message) => (
-                        <div 
-                            key={message.content}
-                            className={cn('p-8 w-full flex items-start gap-x-8 rounded-lg', message.role === 'user' ? 'bg-white border border-black/10' : 'bg-muted')}
+            <FormField
+              control={form.control}
+              name="amount"
+              render={({ field }) => (
+                <FormItem className="col-span-12 lg:col-span-2">
+                  <Select 
+                    disabled={isLoading} 
+                    onValueChange={field.onChange} 
+                    value={field.value} 
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue defaultValue={field.value} />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {amountOptions.map((option) => (
+                        <SelectItem 
+                          key={option.value} 
+                          value={option.value}
                         >
-                            {message.role === 'user' ? <UserAvatar /> : <BotAvatar />}
-                            <p className="text-sm">{message.content}</p>
-                        </div>
-                    ))}
-                </div>
-            </div>
+                          {option.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="resolution"
+              render={({ field }) => (
+                <FormItem className="col-span-12 lg:col-span-2">
+                  <Select 
+                    disabled={isLoading} 
+                    onValueChange={field.onChange} 
+                    value={field.value} 
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue defaultValue={field.value} />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {resolutionOptions.map((option) => (
+                        <SelectItem 
+                          key={option.value} 
+                          value={option.value}
+                        >
+                          {option.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </FormItem>
+              )}
+            />
+            <Button className="col-span-12 lg:col-span-2 w-full" type="submit" disabled={isLoading} size="icon">
+              Gerar
+            </Button>
+          </form>
+        </Form>
+        {isLoading && (
+          <div className="p-20">
+            <Loader />
+          </div>
+        )}
+        {photos.length === 0 && !isLoading && (
+          <Empty label="No images generated." />
+        )}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-8">
+          {photos.map((src) => (
+            <Card key={src} className="rounded-lg overflow-hidden">
+              <div className="relative aspect-square">
+                <Image
+                  fill
+                  alt="Generated"
+                  src={src}
+                />
+              </div>
+              <CardFooter className="p-2">
+                <Button onClick={() => window.open(src)} variant="secondary" className="w-full">
+                  <Download className="h-4 w-4 mr-2" />
+                  Download
+                </Button>
+              </CardFooter>
+            </Card>
+          ))}
         </div>
-    );
-};
-
-export default ImagePage;
+      </div>
+    </div>
+   );
+}
+ 
+export default PhotoPage;
