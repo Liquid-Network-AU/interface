383243dbf35fb34d9906b41c23d30ef75cc424eb
Liam Arbuckle <liam@skinetics.tech>
2023-11-12 12:05:17 +1100

    ⛪️🦅 ↝ It was you who took my hand

---
+++ components/dashboard/Sidebar.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import NavLinks from "@/constants/SidebarLinks";
+
+import { cn } from "@/lib/utils";
+import DarkModeToggle from "../ToggleDarkMode";
+import SignOut from "../auth/Signout";
+
+export default function Sidebar() {
+    return (
+        <SidebarComp className="hidden lg-block dark:bg-gradient-dark flex-1" />
+    )
+}
+
+export const SidebarComp = ({ className }: { className?: string }) => {
+    return (
+        <div className={className}>
+			<div
+				className={cn(
+					"h-full w-full lg:w-96 lg:p-10 space-y-5 lg:border-r flex flex-col "
+				)}
+			>
+				<div className="flex-1 space-y-5">
+					<div className="flex items-center gap-2 flex-1">
+						<h1 className="text-3xl font-bold">Daily Todo</h1>
+
+						<DarkModeToggle />
+					</div>
+					<NavLinks />
+				</div>
+				<div className="">
+					<SignOut />
+				</div>
+			</div>
+		</div>
+    );
+}
