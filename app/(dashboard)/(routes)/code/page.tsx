8b6d9d429230a455623b9649956a356cd13be7f5
Liam Arbuckle <liam@skinetics.tech>
2023-11-13 12:24:52 +1100

    🍥🦋 ↝ As far as the eye can see...abandoned...

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react";
+import { useState } from "react"; 
 import { useRouter } from "next/navigation";
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Code } from "lucide-react";
